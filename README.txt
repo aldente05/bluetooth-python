775d92863ecc0b4824b99a4fbbd7de8f54024ebf
Aldente05 <dex.weldes@gmail.com>
2016-07-23 23:08:19 +0700

    First commit

---
+++ README.txt
@@ -0,0 +1,6 @@
+install python3 python3-dev
+install pip (package/dependency manager)
+
+Under Project Folder
+type this command in terminal :
+sudo pip install -U -r requirement.txt
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Raspberry Android',
+    version='0.1dev',
+    packages=['Resources',],
+    license='MIT',
+    long_description=open('README.txt').read(),
+)
